romannurik/dashclock

commit 4ba7154e2a1c33a4a2f1577a9a6bac2e7a9d7836

Avoid concurrent modification exceptions by returning a copy of the available extensions array
Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

api/src/main/java/com/google/android/apps/dashclock/api/host/DashClockHost.java
@@ -208,13 +208,12 @@ public abstract class DashClockHost {
      */
     public List<ExtensionListing> getAvailableExtensions(boolean onlyWorldReadableExtensions) {
         if (!onlyWorldReadableExtensions) {
-            return mAvailableExtensions;
+            return new ArrayList<>(mAvailableExtensions);
         }
-        List<ExtensionListing> eis = new ArrayList<>();
-        for (ExtensionListing ei : mAvailableExtensions) {
-            if (ei.worldReadable()) {
-                eis.add(ei);
-            }
+        List<ExtensionListing> eis = new ArrayList<>(mAvailableExtensions);
+        int count = eis.size() - 1;
+        for (int i = count - 1; i >= 0; i++) {
+            eis.remove(i);
         }
         return eis;
     }

commit 635fc1fef75203aba67a5e720a4a00f00232ba8a

Fix incorrect for logic introduced in 4ba7154
Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

api/src/main/java/com/google/android/apps/dashclock/api/host/DashClockHost.java
@@ -212,8 +212,11 @@ public abstract class DashClockHost {
         }
         List<ExtensionListing> eis = new ArrayList<>(mAvailableExtensions);
         int count = eis.size() - 1;
-        for (int i = count - 1; i >= 0; i++) {
-            eis.remove(i);
+        for (int i = count; i >= 0; i--) {
+            ExtensionListing ei = eis.get(i);
+            if (!ei.worldReadable()) {
+                eis.remove(i);
+            }
         }
         return eis;
     }