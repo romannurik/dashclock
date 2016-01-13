romannurik/dashclock

commit 949f1241b170177edda1521e494f60d79befe492

Set the mFragment instance onAttach to prevent that it is null.

main/src/main/java/com/google/android/apps/dashclock/configuration/BaseSettingsActivity.java
@@ -65,6 +65,11 @@ public abstract class BaseSettingsActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onAttachFragment(android.app.Fragment fragment) {
+        mFragment = (ExtensionPreferenceFragment) fragment;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             // TODO: if the previous activity on the stack isn't a ConfigurationActivity,

commit a539756b61e4c8e490b617a8c4ac51c52e2004c1

Fix a casting bug introduced in 949f124

main/src/main/java/com/google/android/apps/dashclock/configuration/BaseSettingsActivity.java
@@ -66,7 +66,11 @@ public abstract class BaseSettingsActivity extends AppCompatActivity {
 
     @Override
     public void onAttachFragment(android.app.Fragment fragment) {
-        mFragment = (ExtensionPreferenceFragment) fragment;
+        if (fragment instanceof ExtensionPreferenceFragment) {
+            // instanceof check necessary as not all attached fragments
+            // are ExtensionPreferenceFragments (e.g. app chooser dialog fragments)
+            mFragment = (ExtensionPreferenceFragment) fragment;
+        }
     }
 
     @Override